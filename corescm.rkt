ad227aa56b70601441f0cf80ecdedac58ce6714b
zaoqi <zaomir@outlook.com>
2017-10-04 22:39:04 +0800

    编译器可以实现vector

--- corescm.rkt
+++ corescm.rkt
@@ -14,67 +14,71 @@
 ;;  You should have received a copy of the GNU Affero General Public License
 ;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 (require "alexpander.rkt")
-(set-null-prog!
- '((define-syntax define-syntax-rule
-     (syntax-rules ()
-       ((_ (f . args) x) (define-syntax f
-                           (syntax-rules ()
-                             ((_ . args) x))))))
-   (define-syntax-rule (λ . xs) (lambda . xs))
-   (let-syntax ((def define))
-     (define-syntax define
+(define (init fe)
+  (set-null-prog!
+   '((define-syntax define-syntax-rule
        (syntax-rules ()
-         ((_ var init) (def var init))
-         ((_ (var . args) . body) (define var (λ args . body))))))
-   )
- '((define (zero? x) (eq? x 0))
-   (define (append xs ys)
-     (if (null? xs)
-         ys
-         (cons (car xs) (append (cdr xs) ys))))
-   (define (list . xs)
-     xs)
-   (define (map f xs)
-     (if (null? xs)
-         '()
-         (cons (f (car xs)) (map f (cdr xs)))))
-   (define (filter f xs)
-     (if (null? xs)
-         '()
-         (if (f (car xs))
-             (cons (car xs) (filter f (cdr xs)))
-             (filter f (cdr xs)))))
-   (define (foldl f x xs)
-     (if (null? xs)
-         x
-         (foldl f (f (car xs) x) (cdr xs))))
-   (define (length xs)
-     (if (null? xs)
-         0
-         (+ 1 (length (cdr xs)))))
-   (define (equal? x y)
-     (if (pair? x)
-         (and (pair? y)
-              (equal? (car x) (car y))
-              (equal? (cdr x) (cdr y)))
-         (eq? x y)))
-   (define eqv? equal?)
-   (define-syntax-rule (vec) '_vector_)
-   (define (vector . xs) (cons (vec) xs))
-   (define (vector? x) (and (pair? x) (eq? (car x) (vec))))
-   (define (list->vector xs) (cons (vec) xs))
-   (define (vector->list x) (if (vector? x) (cdr x) (error "vector->list: isn't vector?" x)))
-   (define (vector-ref v k)
-     (cond
-       [(< k 0) (error "vector-ref: isn't exact-nonnegative-integer?" v k)]
-       [(vector? v) (%vector-ref v k (cdr v) k)]
-       [else (error "vector-ref: isn't vector?" v k)]))
-   (define (%vector-ref v k xs x)
-     (cond
-       [(null? xs) (error "vector-ref: index is out of range" v k)]
-       [(zero? x) (car xs)]
-       [(< k 0) (error "vector-ref: isn't exact-nonnegative-integer?" v k)]
-       [else (%vector-ref v k (cdr xs) (- x 1))]))
-   )
- '())
+         ((_ (f . args) x) (define-syntax f
+                             (syntax-rules ()
+                               ((_ . args) x))))))
+     (define-syntax-rule (λ . xs) (lambda . xs))
+     (let-syntax ((def define))
+       (define-syntax define
+         (syntax-rules ()
+           ((_ var init) (def var init))
+           ((_ (var . args) . body) (define var (λ args . body))))))
+     )
+   `((define (zero? x) (eq? x 0))
+     (define (append xs ys)
+       (if (null? xs)
+           ys
+           (cons (car xs) (append (cdr xs) ys))))
+     (define (list . xs)
+       xs)
+     (define (map f xs)
+       (if (null? xs)
+           '()
+           (cons (f (car xs)) (map f (cdr xs)))))
+     (define (filter f xs)
+       (if (null? xs)
+           '()
+           (if (f (car xs))
+               (cons (car xs) (filter f (cdr xs)))
+               (filter f (cdr xs)))))
+     (define (foldl f x xs)
+       (if (null? xs)
+           x
+           (foldl f (f (car xs) x) (cdr xs))))
+     (define (length xs)
+       (if (null? xs)
+           0
+           (+ 1 (length (cdr xs)))))
+     (define (equal? x y)
+       (if (pair? x)
+           (and (pair? y)
+                (equal? (car x) (car y))
+                (equal? (cdr x) (cdr y)))
+           (eq? x y)))
+     (define eqv? equal?)
+     ,@(if (set-member? fe 'vector)
+           '()
+           '((define-syntax-rule (vec) '_vector_)
+             (define (vector . xs) (cons (vec) xs))
+             (define (vector? x) (and (pair? x) (eq? (car x) (vec))))
+             (define (list->vector xs) (cons (vec) xs))
+             (define (vector->list x) (if (vector? x) (cdr x) (error "vector->list: isn't vector?" x)))
+             (define (vector-ref v k)
+               (cond
+                 [(< k 0) (error "vector-ref: isn't exact-nonnegative-integer?" v k)]
+                 [(vector? v) (%vector-ref v k (cdr v) k)]
+                 [else (error "vector-ref: isn't vector?" v k)]))
+             (define (%vector-ref v k xs x)
+               (cond
+                 [(null? xs) (error "vector-ref: index is out of range" v k)]
+                 [(zero? x) (car xs)]
+                 [(< k 0) (error "vector-ref: isn't exact-nonnegative-integer?" v k)]
+                 [else (%vector-ref v k (cdr xs) (- x 1))]))))
+     )
+   '()))
+(init (set))
 (expand-program '())
