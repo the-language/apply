f1dbb873d4afc4326fae9b4190c8eea3e2f6e250
zaoqi <zaomir@outlook.com>
2017-10-04 22:08:17 +0800

    vector

--- corescm.rkt
+++ corescm.rkt
@@ -27,7 +27,8 @@
          ((_ var init) (def var init))
          ((_ (var . args) . body) (define var (λ args . body))))))
    )
- '((define (append xs ys)
+ '((define (zero? x) (eq? x 0))
+   (define (append xs ys)
      (if (null? xs)
          ys
          (cons (car xs) (append (cdr xs) ys))))
@@ -37,12 +38,43 @@
      (if (null? xs)
          '()
          (cons (f (car xs)) (map f (cdr xs)))))
+   (define (filter f xs)
+     (if (null? xs)
+         '()
+         (if (f (car xs))
+             (cons (car xs) (filter f (cdr xs)))
+             (filter f (cdr xs)))))
+   (define (foldl f x xs)
+     (if (null? xs)
+         x
+         (foldl f (f (car xs) x) (cdr xs))))
+   (define (length xs)
+     (if (null? xs)
+         0
+         (+ 1 (length (cdr xs)))))
    (define (equal? x y)
      (if (pair? x)
          (and (pair? y)
               (equal? (car x) (car y))
               (equal? (cdr x) (cdr y)))
          (eq? x y)))
-   (define eqv? equal?))
+   (define eqv? equal?)
+   (define-syntax-rule (vec) '_vector_)
+   (define (vector . xs) (cons (vec) xs))
+   (define (vector? x) (and (pair? x) (eq? (car x) (vec))))
+   (define (list->vector xs) (cons (vec) xs))
+   (define (vector->list x) (if (vector? x) (cdr x) (error "vector->list: isn't vector?" x)))
+   (define (vector-ref v k)
+     (cond
+       [(< k 0) (error "vector-ref: isn't exact-nonnegative-integer?" v k)]
+       [(vector? v) (%vector-ref v k (cdr v) k)]
+       [else (error "vector-ref: isn't vector?" v k)]))
+   (define (%vector-ref v k xs x)
+     (cond
+       [(null? xs) (error "vector-ref: index is out of range" v k)]
+       [(zero? x) (car xs)]
+       [(< k 0) (error "vector-ref: isn't exact-nonnegative-integer?" v k)]
+       [else (%vector-ref v k (cdr xs) (- x 1))]))
+   )
  '())
 (expand-program '())
