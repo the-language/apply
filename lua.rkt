7deaaae703e96e4411afecb4b45c8353b142d495
zaoqi <zaomir@outlook.com>
2017-10-07 18:04:21 +0800

    mv corescm zscm;mv corescm.rkt zscm.rkt

--- lua.rkt
+++ lua.rkt
@@ -14,7 +14,7 @@
 ;;  You should have received a copy of the GNU Affero General Public License
 ;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;(provide c)
-(require "corescm.rkt")
+(require "zscm.rkt")
 (define-syntax %newns
   (syntax-rules ()
     [(_) '()]
