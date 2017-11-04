e7ce8f0fe597ea019ab5e8e8d1d36c07a08acaf2
zaoqi <zaomir@outlook.com>
2017-11-04 10:44:39 +0800

    Fix begin

--- zscm.rkt
+++ zscm.rkt
@@ -726,21 +726,20 @@
     [(? pair? x) (or (GCfind? s (car x)) (GCfind? s (cdr x)))]
     [_ #f]))
 (define (BEGINgc xs)
-  (let ([lastv (last xs)])
+  (let* ([lastv (last xs)] [lastvoid (or (equal? lastv '(void)) (define? lastv))])
     (let ([xs (filter-not (λ (x) (equal? x '(void))) xs)])
       (let-values ([(rdefs rnotdefs) (partition define? xs)])
         (let ([defs (map (λ (x) (cons (second x) (third x))) rdefs)])
-          (let-values ([(marked rest) (partition (λ (x) (GCfind? (car x) rnotdefs)) defs)])
+          (let-values ([(marked rest)
+                        (partition (λ (x)
+                                     (or (and lastvoid (eq? (car x) 'void))
+                                         (GCfind? (car x) rnotdefs))) defs)])
             (let loop ([marked marked] [rest rest])
               (if (null? rest)
                   xs
                   (let-values ([(new newrest) (partition (λ (x) (GCfind? (car x) marked)) rest)])
                     (if (null? new)
-                        (let* ([lastvoid (or (equal? lastv '(void)) (define? lastv))]
-                               [marked0 (map car marked)]
-                               [marked (if lastvoid
-                                           (cons 'void marked0)
-                                           marked0)])
+                        (let ([marked (map car marked)])
                           (let ([xs (filter
                                      (λ (x)
                                        (if (define? x)
