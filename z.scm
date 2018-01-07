666d59c6eafd4caa74f4a84e0c5ffb548f065524
zaoqi <zaomir@outlook.com>
2018-01-07 16:39:42 +0800

    .

--- z.scm
+++ z.scm
@@ -21,15 +21,15 @@
 (define null-set (set))
 (define null-hash (hash))
 ;---------------------------
-(define ($var/k state x k) (k state `(!var ,x)))
-(define ($num/k state x k) (k state `(!num ,x)))
-(define ($char/k state x k) (k state `(!char ,x)))
-(define ($str/k state x k) (k state `(!str ,x)))
-(define ($$val/k state x k) (k state (list x)))
-(define ($$define/k state f x k) (k state `((!def ,f ,x))))
+(define ($var/k local-state state x k) (k local-state state `(!var ,x)))
+(define ($num/k local-state state x k) (k local-state state `(!num ,x)))
+(define ($char/k local-state state x k) (k local-state state `(!char ,x)))
+(define ($str/k local-state state x k) (k local-state state `(!str ,x)))
+(define ($$val/k local-state state x k) (k local-state state (list x)))
+(define ($$define/k local-state state f x k) (k local-state state `((!def ,f ,x))))
 (define $void `!void)
-(define ($$if/k state b xs x ys y k) (k state '() `(!if ,b (begin ,@xs ,x) (begin ,@ys y))))
-(define ($$apply/k state f args k) (k state '() `(!app ,f ,@args)))
+(define ($$if/k local-state state b xs x ys y k) (k local-state state '() `(!if ,b (begin ,@xs ,x) (begin ,@ys y))))
+(define ($$apply/k local-state state f args k) (k local-state state '() `(!app ,f ,@args)))
 ;-----------------------------
 (define-record-type macro
   (macro src proc)
@@ -40,52 +40,52 @@
   (just x)
   just?
   (x run-just))
-(define (macroexpand local-state env state x k) ; (k env state x)
+(define (macroexpand local-state env state x k) ; (k local-state env state x)
   (if (pair? x)
       (let ([f (car x)] [args (cdr x)])
         (let ([m (hash-ref env f #f)])
           (if (and m (macro? m))
               ((macro-proc m)
                local-state env state args
-               (λ () (k env state x))
-               (λ (env state x)
+               (λ () (k local-state env state x))
+               (λ (local-state env state x)
                  (macroexpand local-state env state x k)))
-              (k env state x))))
-      (k env state x)))
+              (k local-state env state x))))
+      (k local-state env state x)))
 (define null-env
   (hash
    'DEFMACROz (macro
                '(λ (local-state env state args default k)
-                  (k (hash-set env (first args) (macro (second args) (EVAL (second args)))) state 'VOIDz))
+                  (k local-state (hash-set env (first args) (macro (second args) (EVAL (second args)))) state 'VOIDz))
                (λ (local-state env state args default k)
-                 (k (hash-set env (first args) (macro (second args) (EVAL (second args)))) state 'VOIDz)))
+                 (k local-state (hash-set env (first args) (macro (second args) (EVAL (second args)))) state 'VOIDz)))
    'define (macro
             '(λ (local-state env state args default k)
                (let ([f (car args)] [r (cdr args)])
                  (if (pair? f)
-                     (k env state `(define ,(car f) (λ ,(cdr f) ,@r)))
+                     (k local-state env state `(define ,(car f) (λ ,(cdr f) ,@r)))
                      (let ([v (car r)])
                        (if (symbol? v)
                            ($var/k
-                            state v
-                            (λ (state r)
-                              (k (hash-set env f r) state 'VOIDz)))
+                            local-state state v
+                            (λ (local-state state r)
+                              (k local-state (hash-set env f r) state 'VOIDz)))
                            (default))))))
             (λ (local-state env state args default k)
               (let ([f (car args)] [r (cdr args)])
                 (if (pair? f)
-                    (k env state `(define ,(car f) (λ ,(cdr f) ,@r)))
+                    (k local-state env state `(define ,(car f) (λ ,(cdr f) ,@r)))
                     (let ([v (car r)])
                       (if (symbol? v)
                           ($var/k
-                           state v
-                           (λ (state r)
-                             (k (hash-set env f r) state 'VOIDz)))
+                           local-state state v
+                           (λ (local-state state r)
+                             (k local-state (hash-set env f r) state 'VOIDz)))
                           (default)))))))))
-(define (COMPILE/k local-state env state x k) ; (k state xs x)
+(define (COMPILE/k local-state env state x k) ; (k local-state state xs x)
   (macroexpand
    local-state env state x
-   (λ (env state x)
+   (λ (local-state env state x)
      (cond
        [(pair? x)
         (let ([f (car x)] [args (cdr x)])
@@ -93,36 +93,37 @@
             [(eq? f 'quote) (QUOTE/k local-state env state (first args) k)]
             [(eq? f 'define)
              ($$define/k
-              state (first args) (second args)
-              (λ (state xs)
-                (k state xs $void)))]
+              local-state state (first args) (second args)
+              (λ (local-state state xs)
+                (k local-state state xs $void)))]
             [(eq? f 'begin) (BEGIN/k local-state env state args k)]
+            [(or (eq? f 'lambda) (eq? f 'λ)) !]
             [(eq? f 'if)
              (COMPILE/k
               local-state env state (first args)
-              (λ (state bs b)
+              (λ (local-state state bs b)
                 (COMPILE/k
                  local-state env state (second args)
-                 (λ (state xs x)
+                 (λ (local-state state xs x)
                    (COMPILE/k
                     local-state env state (third args)
-                    (λ (state ys y)
+                    (λ (local-state state ys y)
                       ($$if/k
-                       state b xs x ys y
-                       (λ (state rs r)
-                         (k state (append bs rs) r)))))))))]
+                       local-state state b xs x ys y
+                       (λ (local-state state rs r)
+                         (k local-state state (append bs rs) r)))))))))]
             [else
              (COMPILE/k
               local-state env state f
-              (λ (state fs f)
+              (λ (local-state state fs f)
                 (COMPILE/k*
                  local-state env state args
-                 (λ (state argss args)
+                 (λ (local-state state argss args)
                    ($$apply/k
-                    state f args
-                    (λ (state rs r)
-                      (k state (append fs argss rs) r)))))))]))]
-       [(eq? x 'VOIDz) (k state '() $void)]
+                    local-state state f args
+                    (λ (local-state state rs r)
+                      (k local-state state (append fs argss rs) r)))))))]))]
+       [(eq? x 'VOIDz) (k local-state state '() $void)]
        [else
         ((cond
            [(symbol? x) $var/k]
@@ -130,35 +131,97 @@
            [(char? x) $char/k]
            [(string? x) $str/k]
            [else (error 'compile "invalid syntax" x)])
-         state x
-         (λ (state x)
-           (k state '() x)))]))))
+         local-state state x
+         (λ (local-state state x)
+           (k local-state state '() x)))]))))
 (define (QUOTE/k) (cond))
-(define (BEGIN/k local-state env state xs k) ; (k state xs x)
+(define ($$val/k+ local-state state x k)
+  (if (eq? x $void)
+      (k local-state state '())
+      ($$val/k local-state state x k)))
+(define (BEGIN/k local-state env state xs k) ; (k local-state state xs x)
   (if (null? (cdr xs))
       (COMPILE/k local-state env state (car xs) k)
       (COMPILE/k
        local-state env state (car xs)
-       (λ (state as a)
+       (λ (local-state state as a)
          (BEGIN/k
           local-state env state (cdr xs)
-          (λ (state ds d)
-            (if (eq? $void a)
-                (k state (append as ds) d)
-                ($$val/k
-                 state a
-                 (λ (state xs)
-                   (k state (append as xs ds) d))))))))))
-(define (COMPILE/k* local-state env state xs k) ; (k state xs rs)
+          (λ (local-state state ds d)
+            ($$val/k+
+             local-state state a
+             (λ (local-state state xs)
+               (k local-state state (append as xs ds) d)))))))))
+(define (COMPILE/k* local-state env state xs k) ; (k local-state state xs rs)
   (if (null? (cdr xs))
       (COMPILE/k
        local-state env state (car xs)
-       (λ (state xs x)
-         (k state xs (list x))))
+       (λ (local-state state xs x)
+         (k local-state state xs (list x))))
       (COMPILE/k
        local-state env state (car xs)
-       (λ (state as a)
+       (λ (local-state state as a)
          (COMPILE/k*
           local-state env state (cdr xs)
-          (λ (state xs rs)
-            (k state (append as xs) (cons a rs))))))))
+          (λ (local-state state xs rs)
+            (k local-state state (append as xs) (cons a rs))))))))
+
+(define (COMPILEtail/k local-state env state x k) ; (k local-state state xs)
+  (macroexpand
+   local-state env state x
+   (λ (local-state env state x)
+     (define (X)
+       (COMPILE/k
+        local-state env state x
+        (λ (local-state state xs x)
+          ($$val/k
+           local-state state x
+           (λ (local-state state es)
+             (k local-state state (append xs es)))))))
+     (define (E) (error 'compile "invalid syntax" x))
+     (cond
+       [(pair? x)
+        (let ([f (car x)] [args (cdr x)])
+          (cond
+            [(or (eq? f 'quote) (eq? f 'lambda) (eq? f 'λ)) (X)]
+            [(eq? f 'define) (E)]
+            [(eq? f 'begin) (BEGINtail/k local-state env state args k)]
+            [(eq? f 'if)
+             (COMPILE/k
+              local-state env state (first args)
+              (λ (local-state state bs b)
+                (COMPILEtail/k
+                 local-state env state (second args)
+                 (λ (local-state state xs)
+                   (COMPILEtail/k
+                    local-state env state (third args)
+                    (λ (local-state state ys)
+                      ($$if-tail/k
+                       local-state state b xs ys
+                       (λ (local-state state rs)
+                         (k local-state state (append bs rs))))))))))]
+            [else
+             (COMPILE/k
+              local-state env state f
+              (λ (local-state state fs f)
+                (COMPILE/k*
+                 local-state env state args
+                 (λ (local-state state argss args)
+                   ($$tail-apply/k
+                    local-state state f args
+                    (λ (local-state state rs)
+                      (k local-state state (append fs argss rs))))))))]))]
+       [else (X)]))))
+(define (BEGINtail/k local-state env state xs k) ; (k local-state state xs)
+  (if (null? (cdr xs))
+      (COMPILEtail/k local-state env state (car xs) k)
+      (COMPILE/k
+       local-state env state (car xs)
+       (λ (local-state state as a)
+         (BEGINtail/k
+          local-state env state (cdr xs)
+          (λ (local-state state ds)
+            ($$val/k+
+             local-state state a
+             (λ (local-state state xs)
+               (k local-state state (append as xs ds))))))))))
