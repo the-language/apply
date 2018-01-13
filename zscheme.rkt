4641d6e30bfdbb7c408fcec10f47770fdeb1c1d8
zaoqi <zaomir@outlook.com>
2018-01-13 13:01:06 +0800

    重寫

--- zscheme.rkt
+++
@@ -1,8 +0,0 @@
-(require compatibility/defmacro)
-(require srfi/9) ; R7RS
-(require racket/sandbox)
-(define EVAL (make-evaluator 'racket))
-(define null-set (set))
-(define null-hash (hash))
-(define INCLUDE-LISTz file->list)
-(define-macro (load x) `(include ,x))
