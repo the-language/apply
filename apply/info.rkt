0c5de72bd604abb77ef09fd796ad3aa80b2ad8d3
zaoqi <zaomir@outlook.com>
2018-01-14 19:46:03 +0800

    .

--- apply/info.rkt
+++ apply/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "apply")
-(define deps '("base"))
+(define deps '("racket"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/apply.scrbl" ())))
 (define pkg-desc "Description Here")
